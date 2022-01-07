081feb527d11781cc8dfc4ca1349c1cb989f9907
Diana Trifu <diana_trifu@yahoo.com>
2022-01-07 21:21:57 +0200

    Create and add "Header" component to main-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./components/main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent
+  },
+  {
+    path: 'main-page',
+    component: MainPageComponent
+  },
+  // {
+  //   path: 'client/historical-consumption',
+  //   component: HistoricalConsumptionComponent,
+  //   canActivate: [LoginGuard],
+  //   data: {expectedRole: 'client'}
+  // },
+  {path: '**', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
